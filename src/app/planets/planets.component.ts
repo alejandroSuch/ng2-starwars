8b387e53a4e40a65ac0f351472320ef40ab92e84
Alejandro Such <alejandro.such@gmail.com>
2017-01-12 19:41:17 +0100

    Planet list

--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { SwapiService } from '../core/swapi.service';
 
 @Component({
-  selector: 'app-planets',
+  selector   : 'app-planets',
   templateUrl: './planets.component.html',
-  styleUrls: ['./planets.component.css']
+  styleUrls  : ['./planets.component.css']
 })
 export class PlanetsComponent implements OnInit {
+  private planets: any[] = [];
+  private currentPage: number = 1;
 
-  constructor() { }
+  constructor (private swapiService: SwapiService) {
+  }
 
-  ngOnInit() {
+  ngOnInit () {
+    this.swapiService
+        .getPlanets(this.currentPage)
+        .then(({ results, page, pages }) => {
+          this.planets = results;
+        });
   }
 
 }
